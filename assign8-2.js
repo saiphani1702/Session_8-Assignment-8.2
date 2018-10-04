5c25184e290c3180ead4d323be21add873415a62
karanam sai phani teja <43602536+saiphani1702@users.noreply.github.com>
2018-10-04 16:13:04 +0530

    Update assign8-2.js

--- assign8-2.js
+++ assign8-2.js
@@ -1,3 +1,5 @@
+//Example using function and callback
+//this function uses callback
 var prepareFood=function(Tablenumber,items,callback){
     console.log("preparing food for items -"+items);
     callback(Tablenumber,items);
@@ -6,4 +8,4 @@ var serveFood=function(Tablenumber,items){
     console.log("serving food for items -"+items);
 
 }
-console.log(prepareFood(1,["Burger","pasta","coke"],serveFood));
+console.log(prepareFood(1,["Burger","pasta","coke"],serveFood));//displays output to the console using callback
